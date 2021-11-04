8a6333c4e6d85620d06ccea0ee5e40ee16b2787e
aidanperez2000 <aidan@perezwitt.com>
2021-11-04 19:45:43 -0400

    Added Adding Feature to AddEvent.  Records new event in database but displays undefined on events page.

--- athletic-form-ui/src/Views/AddEvent.tsx
+++ athletic-form-ui/src/Views/AddEvent.tsx
@@ -3,20 +3,18 @@ import { useState, useEffect } from 'react';
 import { getAllEvents, addEventHandler } from '../Services/EventService';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Event } from '../Models/Event';
 
 interface Props {}
 
 export const AddEvent : React.FC<Props> = () => {
     const [events, setEvents] = useState<any | null>(null);
     const [eventId, setEventId] = useState(0);
-    const [athleticEvent, setAthleticEvent] = useState({ 
-        event : { 
-            sport: "",
-            opponent: "",
-            date: "",
-            time: ""
-        } 
-    });
+    const [sport, setSport] = useState("");
+    const [opponent, setOpponent] = useState("");
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
+    const [homeOrDepart, setHomeOrDepart] = useState("");
 
     useEffect(() => {
 		getAllEvents().then((res) => {
@@ -24,34 +22,50 @@ export const AddEvent : React.FC<Props> = () => {
 		});
     }, []);
 
-    const handleChange = (e: any) => {
-        console.log("Updating value");
-    }
+    console.log(events);
 
     const handleSubmit = () => {
+        setEventId(Math.max(events.map((e: any) => e.Id)) + 1);
+        let event : any = new Event(sport, opponent, date, time, homeOrDepart);
+        addEventHandler(event, eventId).then((res) => {
+            setEvents([...events, res.data]);
+        });
     }
+
     return (
        <Grid>
            <h1>Add Event</h1>
            <form onSubmit = {handleSubmit}>
                <label>
                    Sport:
-                   <input type = "text" value = { athleticEvent.event.sport } onChange = { handleChange }></input>
+                   <input type = "text" value = { sport } 
+                        onChange = { (e: any) => setSport(e.target.value) }>
+                   </input>
                </label>
                <br></br>
                <label>
                    Opponent:
-                   <input type = "text" value = { athleticEvent.event.opponent } onChange = { handleChange }></input>
+                   <input type = "text" value = { opponent } 
+                        onChange = { (e: any) => setOpponent(e.target.value) }>
+                   </input>
                </label>
                <br></br>
                <label>
                    Date:
-                   <input type = "date" value = { athleticEvent.event.date } onChange = { handleChange }></input>
+                   <input type = "date" value = { date } 
+                        onChange = { (e: any) => setDate(e.target.value) }></input>
                </label>
                <br></br>
                <label>
                    Time: 
-                   <input type = "time" value = { athleticEvent.event.time } onChange = { handleChange }></input>
+                   <input type = "time" value = { time } 
+                        onChange = { (e: any) => setTime(e.target.value) }></input>
+               </label>
+               <br></br>
+               <label>
+                   Home/Departure Time
+                   <input type = "text" value = { homeOrDepart } 
+                        onChange = { (e: any) => setHomeOrDepart(e.target.value) }></input>
                </label>
                <br></br>
                <input type = "submit" value="Save" />
