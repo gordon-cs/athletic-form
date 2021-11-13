bea3e265e88bad64ac234616de514b130b3bc072
Hudson Finn <Hudson.Finn@gordon.edu>
2021-11-13 13:29:39 -0500

    Added sql server docker-compose

---
+++ API/api-core/AthleticFormCore/AthleticSQLServer/SQL Queries/AthleticEvent.sql
@@ -0,0 +1,5 @@
+insert into dbo.[AthleticDatabase.AthleticEvents] (Sport, Opponent, HomeOrAway, Destination, EventDate, EventTime) values ('Rowing', 'Endicott', 'Home', 'Gull Pond', '12/28/2021', '2:30 PM');
+insert into dbo.[AthleticDatabase.AthleticEvents] (Sport, Opponent, HomeOrAway, Destination, EventDate, EventTime) values ('Soccer', 'EMCC', 'Away', 'EMCC Soccer Field', '12/28/2021', '5:30 AM');
+insert into dbo.[AthleticDatabase.AthleticEvents] (Sport, Opponent, HomeOrAway, Destination, EventDate, EventTime) values ('Baseball', 'PSU', 'Away', 'PSU Stadium', '12/28/2021', '3:00 AM');
+insert into dbo.[AthleticDatabase.AthleticEvents] (Sport, Opponent, HomeOrAway, Destination, EventDate, EventTime) values ('Tennis', 'UMO', 'Away', 'UMO Courts', '12/24/2021', '2:50 PM');
+insert into dbo.[AthleticDatabase.AthleticEvents] (Sport, Opponent, HomeOrAway, Destination, EventDate, EventTime) values ('Basketball', 'UMF', 'Away', 'UMF Baseball Field', '10/25/2021', '2:30 PM');
---
+++ API/api-core/AthleticFormCore/AthleticSQLServer/SQL Queries/SchemaCreation.sql
@@ -0,0 +1,13 @@
+drop table if exists [AthleticDatabase.AthleticEvents]
+
+-- Create table statements --
+create table [AthleticDatabase.AthleticEvents] (
+    EventID int identity(1,1),
+    Sport varchar(25),
+    Opponent varchar(50),
+    HomeOrAway varchar(4),
+    Destination varchar(30),
+    EventDate Date,
+    EventTime Time,
+    primary key(EventID)
+);
