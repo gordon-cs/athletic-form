d69c33e1d155d3e6932d9000af2dc04214710858
aidanperez2000 <aidan@perezwitt.com>
2021-11-21 13:51:26 -0500

    Added Deleted Events Page

--- athletic-form-ui/src/Views/EventsPage.tsx
+++ athletic-form-ui/src/Views/EventsPage.tsx
@@ -3,7 +3,7 @@ import { getAllEvents } from '../Services/EventService';
 import { useEffect, useState } from 'react';
 import { EventCard } from '../Components/EventCard';
 import { Button, Card, CardActions, CardHeader } from '@mui/material';
-import { FaPlusCircle } from 'react-icons/fa';
+import { FaPlusCircle, FaTrashAlt } from 'react-icons/fa';
 import '../styles/eventsPage.scss';
 import { Link } from 'react-router-dom';
 
@@ -24,38 +24,51 @@ export const EventsPage: React.FC = () => {
 	}, []);
 
 	return (
-		<Grid container spacing={3}>
-			{events == null
-				? 'There are no events to show'
-				: events.map((entry: any) => (
-						<Grid item key={entry['eventId']}>
-							<EventCard
-								eventData={{
-									eventId: entry['eventId'],
-									sport: entry['sport'],
-									opponent: entry['opponent'],
-									date: entry['eventDate'],
-									departOrHome: entry['homeOrAway'],
-									destination: entry['destination'],
-									departureTime: entry['departureTime']
-								}}
-							/>
-						</Grid>
-				  ))}
-			<Card className={'add-card'}>
-				<CardHeader className={'add-header'} title={'Add'}></CardHeader>
-				<CardActions className={'add-action'}>
-					<Link to='/events/add'>
-						<Button
-							size='large'
-							sx={{ backgroundColor: '#710F0F', color: 'white' }}
-							variant={'outlined'}
-						>
-							<FaPlusCircle></FaPlusCircle>
-						</Button>
-					</Link>
-				</CardActions>
-			</Card>
+		<Grid>
+			<h1>Athletic Events</h1>
+			<Link to='/events/deleted'>
+				<Button
+					size='small'
+					sx={{ backgroundColor: '#710F0F', color: 'white' }}
+					variant={'outlined'}
+				>
+					<FaTrashAlt></FaTrashAlt>
+					View Deleted Events
+				</Button>
+			</Link>
+			<Grid container spacing={3}>
+				{events == null
+					? 'There are no events to show'
+					: events.map((entry: any) => (
+							<Grid item key={entry['eventId']}>
+								<EventCard
+									eventData={{
+										eventId: entry['eventId'],
+										sport: entry['sport'],
+										opponent: entry['opponent'],
+										date: entry['eventDate'],
+										departOrHome: entry['homeOrAway'],
+										destination: entry['destination'],
+										departureTime: entry['departureTime']
+									}}
+								/>
+							</Grid>
+					))}
+				<Card className={'add-card'}>
+					<CardHeader className={'add-header'} title={'Add'}></CardHeader>
+					<CardActions className={'add-action'}>
+						<Link to='/events/add'>
+							<Button
+								size='large'
+								sx={{ backgroundColor: '#710F0F', color: 'white' }}
+								variant={'outlined'}
+							>
+								<FaPlusCircle></FaPlusCircle>
+							</Button>
+						</Link>
+					</CardActions>
+				</Card>
+			</Grid>
 		</Grid>
 	);
 };
