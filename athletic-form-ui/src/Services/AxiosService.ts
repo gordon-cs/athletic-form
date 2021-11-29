f6f5e1acfa63efe71f05610a7e812d3925583995
Jacob Christopher <jacob.k.christopher@gmail.com>
2021-11-29 12:27:23 -0500

    Axios fix

--- athletic-form-ui/src/Services/AxiosService.ts
+++ athletic-form-ui/src/Services/AxiosService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-	baseURL: 'https://localhost:44346/api',
+	baseURL: 'https://localhost:5001/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
