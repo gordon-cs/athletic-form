72fa16dd478dd3b49eb0818785347396654f247d
Hudson Finn <Hudson.Finn@gordon.edu>
2021-11-30 18:28:37 -0500

    Updated the SQL files

---
+++ API/api-core/AthleticFormCore/AthleticSQLServer/SQL Queries/CreateTable.sql
@@ -0,0 +1,13 @@
+-- Create table
+create table [AthleticDatabase.AthleticEvents] (
+    EventID int identity(1,1),
+    IsDeleted bit default 0 not null,
+    Sport varchar(25),
+    Opponent varchar(50),
+    HomeOrAway varchar(4),
+    Destination varchar(30),
+    EventDate DateTime,
+    DepartureTime DateTime,
+    primary key(EventID)
+);
+
