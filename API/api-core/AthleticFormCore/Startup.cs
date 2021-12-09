b653a440e7930c6ca3a5e6b3af8b6600054281ac
Hudson Finn <Hudson.Finn@gordon.edu>
2021-12-09 11:43:44 -0500

    Cleanup

--- API/api-core/AthleticFormCore/Startup.cs
+++ API/api-core/AthleticFormCore/Startup.cs
@@ -3,12 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration.UserSecrets;
 using Unity;
 using AthleticFormLibrary;
 using AthleticFormLibrary.DataAccess;
 using Microsoft.EntityFrameworkCore;
-using System;
 
 namespace AthleticFormCore
 {
