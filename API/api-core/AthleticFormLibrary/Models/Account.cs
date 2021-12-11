8bb8a97749f950cdbe78d84f83b80f06b873e487
aidanperez2000 <aidan@perezwitt.com>
2021-12-11 17:28:10 -0500

    Added Set to Attributes in Account

--- API/api-core/AthleticFormLibrary/Models/Account.cs
+++ API/api-core/AthleticFormLibrary/Models/Account.cs
@@ -5,10 +5,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace AthleticFormLibrary.Models {
     [Table("AthleticDatabase.Account", Schema ="dbo")]
     public class Account {
-        public string Gordon_ID { get; }
-        public string NickName { get; }
-        public string FirstName { get; }
-        public string LastName { get; }
-        public string Email { get; }
+        public string Gordon_ID { get; set; }
+        public string Nickname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
