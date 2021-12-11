1b578de293880d5e1520ea25885d011adb0cdc8c
aidanperez2000 <aidan@perezwitt.com>
2021-12-10 19:43:00 -0500

    Changed name of begin_tim to CourseBeginTime in Conflicts View

--- API/api-core/AthleticFormCore/AthleticSQLServer/SQL Queries/CreateConflictsView.sql
+++ API/api-core/AthleticFormCore/AthleticSQLServer/SQL Queries/CreateConflictsView.sql
@@ -4,7 +4,7 @@
 */
 
 create view Conflicts as (
-    select distinct top 1000 ie.EventID, acc.Email, ae.DepartureTime, sched.begin_tim, h.crs_cde as CourseCode from    
+    select distinct top 1000 ie.EventID, acc.Email, ae.DepartureTime, sched.begin_tim as CourseBeginTime, h.crs_cde as CourseCode from    
                 dbo.[AthleticDatabase.InEvent] ie
                 inner join dbo.[AthleticDatabase.Student_Crs_Hist] h on h.Id_Num = ie.Gordon_ID
                 inner join dbo.[AthleticDatabase.Section_Schedules] sched on  sched.CRS_CDE = h.crs_cde
