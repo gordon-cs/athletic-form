0c4db531ce1d12f87c8dcca66ccc850e9acb8bbd
Jacob Christopher <jacob.k.christopher@gmail.com>
2022-01-28 21:53:31 -0500

    Added scheduler to automate emails
    Pushing the code for the scheduler. Not committing my current experimentation on how to run as part of startup for the time being, so no actual change will occur to anything Aidan is working on here.

---
+++ API/api-core/AthleticFormLibrary/Scheduler/EmailScheduler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using AthleticFormLibrary.Emailer;
+
+namespace AthleticFormLibrary.Scheduler
+{
+    public class EmailScheduler
+    {
+        private static EmailScheduler _instance;
+        private List<Timer> timers = new List<Timer>();
+
+        private EmailScheduler() { }
+
+        public static EmailScheduler Instance => _instance ?? (_instance = new EmailScheduler());
+
+        public void ScheduleWeeklyTask(DateTime firstRun)
+        {
+            DateTime now = DateTime.Now;
+            if (now > firstRun) {
+                firstRun = firstRun.AddDays(7);
+            }
+
+            TimeSpan timeRemaining = firstRun - now;
+            if (timeRemaining <= TimeSpan.Zero) {
+                timeRemaining = TimeSpan.Zero;
+            }
+
+            var timer = new Timer(x => {
+                EmailClient emailer = new EmailClient();
+                emailer.SendMail("");
+            }, null, timeRemaining, TimeSpan.FromDays(7));
+
+            timers.Add(timer);
+        }
+
+        public void ScheduleTestTask(DateTime firstRun)
+        {
+            DateTime now = DateTime.Now;
+            if (now > firstRun) {
+                firstRun = firstRun.AddMinutes(2);
+            }
+
+            TimeSpan timeRemaining = firstRun - now;
+            if (timeRemaining <= TimeSpan.Zero) {
+                timeRemaining = TimeSpan.Zero;
+            }
+
+            var timer = new Timer(x => {
+                System.Diagnostics.Debug.WriteLine("Scheduled Task executing at: " + DateTime.Now);
+                EmailClient emailer = new EmailClient();
+                emailer.SendMail("");
+            }, null, timeRemaining, TimeSpan.FromMinutes(2));
+
+            timers.Add(timer);
+        }
+    }
+}
