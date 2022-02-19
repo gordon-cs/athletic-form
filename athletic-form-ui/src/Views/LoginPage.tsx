77ad98bbfccc1dae505b6c240b7a3acf816cad8a
Jacob Christopher <jacob.k.christopher@gmail.com>
2022-02-19 16:58:44 -0500

    Storing token to local storage

--- athletic-form-ui/src/Views/LoginPage.tsx
+++ athletic-form-ui/src/Views/LoginPage.tsx
@@ -1,6 +1,5 @@
 import { Grid } from '@mui/material';
 import { Button, Box } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
 import '../styles/login.scss';
@@ -10,23 +9,6 @@ export const LoginPage: React.FC = () => {
 
     const [username, setUsername] = useState<any | null>(null);
     const [password, setPassword] = useState<any | null>(null);
-
-    const parseResponse = (res: any) => {
-        // Parse body of response if not empty
-        //    Make sure text of response is not empty before trying to convert it
-        //    to a JSON object
-        const json = res
-          .text()
-          .then((text: string) => (text.length ? JSON.parse(text) : {}));
-          // Handle error if response body is not valid JSON
-          //.catch((err) => Promise.reject(createError(err, res)));
-      
-        // Handle error when response body is valid but status code is not
-        // if (!res.ok) {
-        //   return json.then((data) => Promise.reject(createError(data, res)));
-        // }
-        return json;
-      };
 
     const handleLogin = async () => {
 		console.log("User attempted to log into the application.");
@@ -48,9 +30,12 @@ export const LoginPage: React.FC = () => {
         let token = await fetch(request).then(response => response.json()).then(data => data.access_token);
 
         try {
-            console.log("Token: ", token.toString());
+            console.log("Bearer ", token.toString());
+            // Store the token
+            localStorage.setItem('token', token.toString());
+
             if( token.toString().startsWith('ey') ){
-                console.log("condition");
+                // Redirect to home page
                 window.location.href = "/events";
             }
         } catch {
