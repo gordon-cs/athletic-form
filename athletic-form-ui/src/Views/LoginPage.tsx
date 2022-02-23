9268accf27563d5fc28569f2a4aa2b5f3b8d0cc9
Jacob Christopher <jacob.k.christopher@gmail.com>
2022-02-22 23:52:38 -0500

    Added error catching and storage of email
    - Post is now not working so need to dig in more but committing at this point

--- athletic-form-ui/src/Views/LoginPage.tsx
+++ athletic-form-ui/src/Views/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material';
+import { Grid, Alert } from '@mui/material';
 import { Button, Box } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import { useState } from 'react';
@@ -7,18 +7,37 @@ import '../styles/login.scss';
 
 export const LoginPage: React.FC = () => {
 
-    const [username, setUsername] = useState<any | null>(null);
+    const [email, setEmail] = useState<any | null>(null);
     const [password, setPassword] = useState<any | null>(null);
+    const [showAlert, setShowAlert] = useState(false);
+    const [usernameError, setUsernameError] = useState(false);
 
     const handleLogin = async () => {
 		console.log("User attempted to log into the application.");
-
+        // Username set in try block
+        let username = email;
+        var num = 1;
+        try {
+            const usernameArray = email.split("@");
+            username = usernameArray[0];
+            console.log(usernameArray[1]);
+            // If not a valid email
+            if (usernameArray[1] !== 'gordon.edu') { throw new Error("Invalid email address.")}
+            console.log("?")
+        } catch {
+            setShowAlert(false);
+            setUsernameError(true);
+            setTimeout(() => {
+                setUsernameError(false)
+            }, 12000);
+            return;
+        }
         const loginInfo = new URLSearchParams({
             username: username,
             password: password,
             grant_type: 'password',
         });
-        
+        console.log(username);
         const request = new Request(`https://360Api.gordon.edu/token`, {
             method: 'post',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -27,19 +46,27 @@ export const LoginPage: React.FC = () => {
             body: loginInfo,
         });
         
-        let token = await fetch(request).then(response => response.json()).then(data => data.access_token);
+        let response = await fetch(request);
+        let token = response.json().then(data => data.access_token);
 
-        try {
+        if(response.status === 200) {
             console.log("Bearer ", token.toString());
             // Store the token
             localStorage.setItem('token', token.toString());
-
+            localStorage.setItem('email', email);
             if( token.toString().startsWith('ey') ){
                 // Redirect to home page
                 window.location.href = "/events";
             }
-        } catch {
-            alert("Invalid credentials!");
+        }
+        else // Failed to authenticate
+        {
+            setUsernameError(false);
+            setShowAlert(true);
+            setTimeout(() => {
+                setShowAlert(false)
+            }, 12000);
+
         }
 	};
 
@@ -53,10 +80,10 @@ export const LoginPage: React.FC = () => {
                     <h1>Athletic Form</h1>
 
                     <TextField
-					value={username}
-                    label='Username'
+					value={email}
+                    label='Email'
 					onChange={(e: any) => {
-						setUsername(e.target.value);
+						setEmail(e.target.value);
 					}}
                     />
                     <TextField
@@ -74,6 +101,9 @@ export const LoginPage: React.FC = () => {
                         size='large'
                         className="button"
                     >Login</Button>
+                    <h1> </h1>
+                    { showAlert && (<Alert severity="error" >Your username or password was incorrect.</Alert>)}
+                    { usernameError && (<Alert severity="error" >Please enter a valid Gordon email address.</Alert>)}
                 </Box>
             </Grid>
     )
