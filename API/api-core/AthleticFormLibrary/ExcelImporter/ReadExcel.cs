d03cfe4e04fd18df74c9af679b43534d3033c6fa
Josiah Kangas <jkangas77@gmail.com>
2022-03-14 11:18:00 -0400

    Imported the excel reader library

--- API/api-core/AthleticFormLibrary/ExcelImporter/ReadExcel.cs
+++ API/api-core/AthleticFormLibrary/ExcelImporter/ReadExcel.cs
@@ -1,1 +1,16 @@
 //Important link for this: https://github.com/ExcelDataReader/ExcelDataReader
+
+using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+{
+    // Auto-detect format, supports:
+    //  - Binary Excel files (2.0-2003 format; *.xls)
+    //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+    using (var reader = ExcelReaderFactory.CreateReader(stream))
+    {
+
+        Use the AsDataSet extension method
+        var result = reader.AsDataSet();
+
+        // The result of each spreadsheet is in result.Tables
+    }
+}
