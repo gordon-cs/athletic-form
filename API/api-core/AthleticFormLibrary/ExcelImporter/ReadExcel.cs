9cbb4bb361cac551d5a1a27ec699c501d6b4d989
Josiah Kangas <jkangas77@gmail.com>
2022-03-14 12:10:22 -0400

    Reformatting code

--- API/api-core/AthleticFormLibrary/ExcelImporter/ReadExcel.cs
+++ API/api-core/AthleticFormLibrary/ExcelImporter/ReadExcel.cs
@@ -1,16 +1,33 @@
 //Important link for this: https://github.com/ExcelDataReader/ExcelDataReader
+//Examples link: https://csharp.hotexamples.com/examples/-/IExcelDataReader/AsDataSet/php-iexceldatareader-asdataset-method-examples.html
 
-using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
-{
-    // Auto-detect format, supports:
-    //  - Binary Excel files (2.0-2003 format; *.xls)
-    //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
-    using (var reader = ExcelReaderFactory.CreateReader(stream))
-    {
+using ExcelDataReader;
+using ExcelDataReader.DataSet;
 
-        Use the AsDataSet extension method
-        var result = reader.AsDataSet();
 
-        // The result of each spreadsheet is in result.Tables
+public class ExcelReader {
+
+    
+    public void ReadExcelFile(string filePath) {
+        using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read)){
+            // Auto-detect format, supports:
+            //  - Binary Excel files (2.0-2003 format; *.xls)
+            //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            {
+
+                Use the AsDataSet extension method
+                var result = reader.AsDataSet();
+
+                // The result of each spreadsheet is in result.Tables
+                foreach (var t in result.Tables) {
+                    Console.WriteLine(t);
+                }
+            }
+        }
+    }
+
+    private void GetTable(string tableName) {
+        
     }
 }
