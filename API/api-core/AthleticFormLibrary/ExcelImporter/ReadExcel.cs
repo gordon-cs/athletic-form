7e64af98d71cdd55c9451c8850824b37cc06a044
Josiah Kangas <jkangas77@gmail.com>
2022-03-14 15:04:13 -0400

    Added some empty helper functions

--- API/api-core/AthleticFormLibrary/ExcelImporter/ReadExcel.cs
+++ API/api-core/AthleticFormLibrary/ExcelImporter/ReadExcel.cs
@@ -7,13 +7,14 @@ using ExcelDataReader.DataSet;
 
 public class ExcelReader {
 
-    
+    private var worksheet;
+
     public void ReadExcelFile(string filePath) {
         using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read)){
             // Auto-detect format, supports:
             //  - Binary Excel files (2.0-2003 format; *.xls)
             //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
-            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            using (worksheet = ExcelReaderFactory.CreateReader(stream))
             {
 
                 Use the AsDataSet extension method
@@ -28,6 +29,18 @@ public class ExcelReader {
     }
 
     private void GetTable(string tableName) {
-        
+        List<DataRow> rows = (from DataRow r in worksheet.Rows Select r).ToList();
+
+    }
+
+    //Upload data to our db
+    private void UpdateDB() {
+
+    }
+
+    //Identify Event
+    //Used for figuring out if our db should be edited or added to
+    private void Id_Event() {
+
     }
 }
