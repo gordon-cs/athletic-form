c97927b5015201caead0b7cb4eb9b114a6fa6d56
Hudson Finn <Hudson.Finn@gordon.edu>
2022-03-28 15:06:59 -0400

    Add theme to index.tsx

--- athletic-form-ui/src/index.tsx
+++ athletic-form-ui/src/index.tsx
@@ -1,12 +1,25 @@
 import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme  } from '@mui/material/styles'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+const theme = createTheme({
+    palette: {  
+        background: {
+            default: "#333333"        
+        }
+    }
+
+})
+
+
 ReactDOM.render(
 	<React.StrictMode>
+    <ThemeProvider theme={theme}>
 		<CssBaseline />    {/*Possibly remove to allow us to be funky with the css. Can't change bg-color currently*/} 
 		<App />
+    </ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
